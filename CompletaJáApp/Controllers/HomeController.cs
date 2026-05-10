f445429f3063a274122c8869f8ebdfb57b1f48e5
Lyu Borges <lyuraynier@gmail.com>
2026-05-10 14:11:27 -0300

    Fiz a conexão com o banco de dados, inclui o campo de foto e CPF no cadastro e fiz a validação de login

--- CompletaJáApp/Controllers/HomeController.cs
+++ CompletaJáApp/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http; // ADICIONADO: Necessário para ler a Session
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompletaJáApp.Controllers
@@ -6,19 +7,25 @@ namespace CompletaJáApp.Controllers
     {
         public IActionResult Index()
         {
-            // COMENTADO TEMPORARIAMENTE PARA TESTES
-            // string usuario = HttpContext.Session.GetString("UsuarioLogado");
-            // if (string.IsNullOrEmpty(usuario))
-            // {
-            //     return RedirectToAction("Index", "Account");
-            // }
+            // 1. Lemos o "crachá" do usuário
+            string usuario = HttpContext.Session.GetString("NomeUsuario");
 
+            // 2. SEGURANÇA: Se o crachá estiver vazio (não logado), chuta de volta pro Login
+            if (string.IsNullOrEmpty(usuario))
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+            // 3. Se passou pela trava, exibe a tela Home normalmente
             return View();
         }
 
         public IActionResult Sair()
         {
+            // Apaga o "crachá" da memória do servidor
             HttpContext.Session.Clear();
+
+            // Redireciona para o Login
             return RedirectToAction("Index", "Account");
         }
     }
