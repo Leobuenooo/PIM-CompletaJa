8d4d600a7fbb5cc586f7b66e67787ac11c34f21f
Leobuenooo <leobueno1906@gmail.com>
2026-05-10 00:59:50 -0300

    Adiciona páginas de cadastro, acessibilidade com api Vlibras
    Foram criadas páginas para adicionar álbuns e locais. Os arquivos de listagem agora redirecionam para essas páginas. Incluído o plugin VLibras para acessibilidade.

--- CompletaJáApp/Controllers/AlbumController.cs
+++ CompletaJáApp/Controllers/AlbumController.cs
@@ -4,7 +4,14 @@ namespace CompletaJáApp.Controllers
 {
     public class AlbumController : Controller
     {
+        // Esta é a página principal de Álbuns
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        // ADICIONE ESTE MÉTODO PARA A NOVA PÁGINA
+        public IActionResult Add()
         {
             return View();
         }
--- CompletaJáApp/Controllers/LocaisController.cs
+++ CompletaJáApp/Controllers/LocaisController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace CompletaJáApp.Controllers
+namespace CompletaJaApp.Controllers
 {
     public class LocaisController : Controller
     {
+        // Abre a página Meus Locais
         public IActionResult Index()
         {
             return View();
+        }
+
+        // Abre a página de Adicionar Local
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // Salva o local e volta pra tela inicial
+        [HttpPost]
+        public IActionResult Criar()
+        {
+            return RedirectToAction("Index");
         }
     }
 }
